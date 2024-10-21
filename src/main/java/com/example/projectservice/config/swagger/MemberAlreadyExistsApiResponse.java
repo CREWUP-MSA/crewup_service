79edcdc9c6090de294e1090bf563a1d2c9a42589
h0ch1 <h0ch1@naver.com>
2024-10-21 17:43:13 +0900

    swagger 설정 커스텀 어노테이션 적용

---
+++ src/main/java/com/example/projectservice/config/swagger/MemberAlreadyExistsApiResponse.java
@@ -0,0 +1,33 @@
+package com.example.projectservice.config.swagger;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.example.projectservice.dto.CustomApiResponse;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(responseCode = "401", description = "이미 존재하는 회원입니다.",
+	content = @Content(
+		schema = @Schema(implementation = CustomApiResponse.class),
+		examples = {
+			@ExampleObject(
+				name = "이미 존재하는 회원입니다.",
+				value = """
+							{
+								"message": "이미 존재하는 회원입니다.",
+							}
+							"""
+			)
+		}
+	)
+)
+public @interface MemberAlreadyExistsApiResponse {
+}
