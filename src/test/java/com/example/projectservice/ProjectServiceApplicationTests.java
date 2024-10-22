0d37a10dab8fbcff78f6ee7653a8cac977c4b055
JunYoung Choi <h0ch1@naver.com>
2024-10-22 20:00:33 +0900

    수정

--- src/test/java/com/example/projectservice/ProjectServiceApplicationTests.java
+++ src/test/java/com/example/projectservice/ProjectServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.projectservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1, topics = {"member-create"})
 class ProjectServiceApplicationTests {
 
 	@Test
