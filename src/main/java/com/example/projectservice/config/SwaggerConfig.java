47f35620a07ec39bb7b09e7f0aafb04357508346
h0ch1 <h0ch1@naver.com>
2024-10-23 17:50:01 +0900

    swagger 통합 설정

--- src/main/java/com/example/projectservice/config/SwaggerConfig.java
+++ src/main/java/com/example/projectservice/config/SwaggerConfig.java
@@ -3,9 +3,11 @@ package com.example.projectservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,11 +15,16 @@ public class SwaggerConfig {
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
-			.info(new Info()
-				.title("Crewup Service API")
-				.version("1.0")
-				.description("API Documentation for Crewup Service")
-				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
-			);
+			.components(new Components())
+			.addServersItem(new Server().url("/"))
+			.info(info());
+	}
+
+	private Info info() {
+		return new Info()
+			.title("Crewup Service API")
+			.version("1.0")
+			.description("API Documentation for Crewup Service")
+			.license(new License().name("Apache 2.0").url("http://springdoc.org"));
 	}
 }
